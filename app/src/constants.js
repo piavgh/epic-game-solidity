4277282f3aab4e95fc43668f69474f85b37929e1
Hoang Trinh <hoang.trinhj@gmail.com>
2021-12-04 18:08:06 +0700

    feat: Add React App

---
+++ app/src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x3ec448302B58d8b211aF90F5bD64F8621623fEa9';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
